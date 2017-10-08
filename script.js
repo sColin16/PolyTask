e3482806bfe81928e7c864cca9f0373ab7c834ec
system_user <csil167593@adams12.org>
2017-10-08 16:05:40 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,52 @@
+/*******
+  ____            _           _____                 _
+ |  _ \    ___   | |  _   _  |_   _|   __ _   ___  | | __
+ | |_) |  / _ \  | | | | | |   | |    / _` | / __| | |/ /
+ |  __/  | (_) | | | | |_| |   | |   | (_| | \__ \ |   <
+ |_|      \___/  |_|  \__, |   |_|    \__,_| |___/ |_|\_\
+                      |___/
+
+Copyright 2017 Colin Siles
+
+script.js
+
+PolyTask Pre-Alpha Phase
+
+********/
+
+window.onLoad = onLoad();
+
+function onLoad(){
+    //append event listeners to navigation buttons
+    console.log("running");
+    var navButtons = document.getElementById("navbar").children;
+    
+    function appendListener(object){
+        object.addEventListener("touchstart", function(){openTab(object.classList[0])});
+    }
+    
+    for(var i = 0, n = navButtons.length; i<n; i++){
+        //use of callback to ensure individual button functionality (due to how javascript passes by reference only for objects)
+        appendListener(navButtons[i]);
+    }
+    
+    openTab("dashboard");
+}
+
+function hideTabs(){
+    var navButtons = document.getElementById("navbar").children;
+    var tabs = document.getElementById("page-wrapper").children;
+    
+    for(var i = 0, n = navButtons.length; i<n; i++){
+        navButtons[i].style.color = "grey";
+        tabs[i].style.display = "none";
+    }
+    
+}
+
+function openTab(name){
+    console.log("opening " + name);
+    hideTabs();
+    document.getElementById("page-wrapper").getElementsByClassName(name)[0].style.display = "block";
+    document.getElementById("navbar").getElementsByClassName(name)[0].style.color = "#e32636";
+}
