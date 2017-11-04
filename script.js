24a3a4cc0e4d92b56ff08366ea51fa7982f970a4
system_user <csil167593@adams12.org>
2017-11-04 14:28:56 -0600

    Initial Commit

--- script.js
+++ script.js
@@ -14,17 +14,66 @@ PolyTask Pre-Alpha Phase
 
 ********/
 
+var scrollThreshold = -200;
+
 window.onLoad = onLoad();
 
 window.addEventListener("scroll", onScroll);
 
+function fromTemplate(templateName){
+    var itemTemplate = document.getElementById(templateName + "-template-wrapper").content;
+    var item = itemTemplate.getElementById(templateName + "-template").cloneNode(true);
+    item.removeAttribute("id");
+    
+    return item;
+}
+
 function onScroll(){
-    console.log(window.scrollY);
-    document.getElementsByClassName("day-counter-number")[0].innerHTML = window.scrollY;
-    if(window.scrollY < -150){
-        prompt("Enter Idea");
-    } else {
+    if(window.scrollY < scrollThreshold){
+        addIdea();
     }
+}
+
+function addIdea(){
+    var ideaName = prompt("Enter idea");
+    if(ideaName === null){
+        console.log("Something went wrong");
+        return;
+    } else{
+        document.getElementById("new-idea-wrapper").style.display = "block";
+        document.getElementById("new-idea-title").value = ideaName;
+    }
+}
+
+function newDashboardBox(primaryColor, secondaryColor, name){
+    var dashboardBox = fromTemplate("dashboard-box");
+    var dashboardBoxName = document.createTextNode(name);
+    
+    var dashboardList = document.getElementById("page-wrapper").getElementsByClassName("dashboard")[0];
+    var buffer = document.getElementById("page-wrapper").getElementsByClassName("dashboard")[0].getElementsByClassName("lower-buffer")[0];
+    
+    dashboardBox.id = name;
+    dashboardBox.getElementsByClassName("dashboard-box-title")[0].classList.add(primaryColor);
+    dashboardBox.getElementsByClassName("dashboard-box-content")[0].classList.add(secondaryColor);
+    
+    dashboardBox.getElementsByClassName("dashboard-box-title")[0].appendChild(dashboardBoxName);
+    
+    dashboardList.insertBefore(dashboardBox, buffer);
+}
+
+function newProjectContainer(dashboardBoxName, name, projectInfo, days){
+    //append a project container from template to a given dashboardBox
+    var dayLabel = "Days";
+    if(days === 1) dayLabel = "Day";
+    
+    var projectName = document.createTextNode(projectName);
+    var dayLabel = document.createTextNode(dayLabel);
+}
+
+function addTask(){
+    
+    
+    
 }
 
 function onLoad(){
@@ -32,32 +81,35 @@ function onLoad(){
     console.log("running");
     var navButtons = document.getElementById("navbar").children;
     
-    function appendListener(object){
+    function appendMainTabListener(object){
         object.addEventListener("touchstart", function(){openTab(object.classList[0])});
     }
     
     for(var i = 0, n = navButtons.length; i<n; i++){
         //use of callback to ensure individual button functionality (due to how javascript passes by reference only for objects)
-        appendListener(navButtons[i]);
+        appendMainTabListener(navButtons[i]);
     }
     
+    
+    
+    
     openTab("dashboard");
+    addIdea();
 }
+
 
 function hideTabs(){
     var navButtons = document.getElementById("navbar").children;
     var tabs = document.getElementById("page-wrapper").children;
-    
     for(var i = 0, n = navButtons.length; i<n; i++){
-        navButtons[i].style.color = "grey";
+        navButtons[i].classList.remove("navbutton-active");
         tabs[i].style.display = "none";
     }
-    
 }
 
 function openTab(name){
     console.log("opening " + name);
     hideTabs();
     document.getElementById("page-wrapper").getElementsByClassName(name)[0].style.display = "block";
-    document.getElementById("navbar").getElementsByClassName(name)[0].style.color = "#e32636";
+    document.getElementById("navbar").getElementsByClassName(name)[0].classList.add("navbutton-active");
 }
